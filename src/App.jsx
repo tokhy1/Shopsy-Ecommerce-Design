1701b2edbacbb833d64008d3ef7238431dc793ae
tokhy24 <mohamed21346587@gmail.com>
2024-07-17 21:12:53 +0300

    Finish Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
