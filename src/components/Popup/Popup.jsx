7a0fa3caa2c4a81f370e1c74364416342030febd
tokhy24 <mohamed21346587@gmail.com>
2024-07-20 02:27:12 +0300

    Update Popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -6,7 +6,7 @@ const Popup = ({ orderPopup, setOrderPopup }) => {
     <>
       {orderPopup && (
         <div className="popup">
-          <div className="h-screen w-screen fixed top-0 left-0 bg-plack/50 z-50 backdrop-blur-sm">
+          <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
             <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 rounded-md duration-200 w-[300px]">
               {/* Header */}
               <div className="flex items-center justify-between">
